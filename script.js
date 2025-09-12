7f786283a439dda858cf22b486dfc3cdd789d7ba
italuanadasilva53-cpu <italuanadasilva53@gmail.com>
2025-09-12 08:33:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-consrt mensagens = [
-  "Você é masis forte do que imagina 💛"
-  "Nunca se esqueça:sua vida importa 🌻"
-  "Você não esta sozinho, estamosa juntos🤝"
-"Acredite: dias melhores virão ⭐"
+const mensagens = [
+  "Você é masis forte do que imagina 💛",
+  "Nunca se esqueça:sua vida importa 🌻",
+  "Você não esta sozinho, estamosa juntos🤝",
+"Acredite: dias melhores virão ⭐",
 "sempre existe esperança 🌈"
 ];
 document.getElementById("btnMensagem").addEventListener("click",() => {
-    const aleatoria = mensagens[Math.floor(Math.random() * mensagens.lengh)];
-    docment.getelementById("mensagem").innerText =aleatoria;
+    const aleatoria = mensagens[Math.floor(Math.random() * mensagens.lenght)];
+    document.getElementById("mensagem").innerText =aleatoria;
 });
